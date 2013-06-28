201c343de03a34d46c80efdb87fb4b5a086aeb06
elizahyde <elizabeth@elizahyde.com>
2013-06-28 10:14:26 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playlistr::Application.config.secret_token = '20139fd1aaf0e57b4baa03567992d794d6f5fd271141a7d8b71d7e5aa93af1cb9c33c588d3ac09076d80c937a2413489bc5a0cbd7c48203471bcee954db91943'
